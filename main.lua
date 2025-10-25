275215d96007f18df590c26272761feef1b23a4f
4pairs <hkjng8n2sf@privaterelay.appleid.com>
2025-10-25 13:45:09 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/4pairs/xenonhub/refs/heads/main/games.lua"))()
 
+local URL = Games[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
